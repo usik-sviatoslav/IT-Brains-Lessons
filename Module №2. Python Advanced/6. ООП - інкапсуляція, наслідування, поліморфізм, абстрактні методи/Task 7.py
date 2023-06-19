a1dc8f690bc3345a13ef27dbd795f09277dd8f80
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-19 03:07:54 +0300

    Modified

--- Module №2. Python Advanced/6. ООП - інкапсуляція, наслідування, поліморфізм, абстрактні методи/Task 7.py
+++ Module №2. Python Advanced/6. ООП - інкапсуляція, наслідування, поліморфізм, абстрактні методи/Task 7.py
@@ -4,3 +4,35 @@
 класу "Rectangle", щоб вони використовували властивість side_length класу "Square". Виведіть значення ширини,
 висоти й довжини сторони для об'єкта класу "Square"."""
 
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_width(self):
+        return f"Ширина: {self.width}"
+
+    def get_height(self):
+        return f"Висота: {self.height}"
+
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
+        self.side_length = side_length
+
+    def get_side_length(self):
+        return f"Довжина: {self.side_length}"
+
+    def get_width(self):
+        return f"Ширина: {self.width}"
+
+    def get_height(self):
+        return f"Висота: {self.height}"
+
+
+rectangle = Square(5)
+print(rectangle.get_width())
+print(rectangle.get_height())
+print(rectangle.get_side_length())
--- Module №2. Python Advanced/6. ООП - інкапсуляція, наслідування, поліморфізм, абстрактні методи/Task 8.py
+++ Module №2. Python Advanced/6. ООП - інкапсуляція, наслідування, поліморфізм, абстрактні методи/Task 8.py
@@ -11,3 +11,66 @@
 Виведіть інформацію про кожен автомобіль, включаючи загальну інформацію з Car та специфічну інформацію для
 кожного класу."""
 
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def get_brand(self):
+        return f"Бренд: {self.brand}"
+
+    def get_model(self):
+        return f"Модель: {self.model}"
+
+    def display_info(self):
+        return f"{Car.get_brand(self)}\n{Car.get_model(self)}"
+
+
+class Sedan(Car):
+    def __init__(self, brand, model, doors):
+        super().__init__(brand, model)
+        self.doors = doors
+
+    def doors_count(self):
+        return f"Кількість дверей: {self.doors}"
+
+    def display_info(self):
+        return f"{Car.display_info(self)}\n{Sedan.doors_count(self)}"
+
+
+class SUV(Car):
+    def __init__(self, brand, model, seats):
+        super().__init__(brand, model)
+        self.seats = seats
+
+    def seats_number(self):
+        return f"Кількість місць: {self.seats}"
+
+    def display_info(self):
+        return f"{Car.display_info(self)}\n{SUV.seats_number(self)}"
+
+
+class SportsCar(Car):
+    def __init__(self, brand, model, speed):
+        super().__init__(brand, model)
+        self.speed = speed
+
+    def max_speed(self):
+        return f"Максимальна швидкість: {self.speed} км/год"
+
+    def display_info(self):
+        return f"{Car.display_info(self)}\n{SportsCar.max_speed(self)}"
+
+
+sedan = Sedan("Toyota", "Camry", 4)
+suv = SUV("Ford", "Explorer", 7)
+sports_car = SportsCar("Ferrari", "488 GTB", 330)
+
+print(33 * "-")
+print(sedan.display_info())
+print(33 * "-")
+print(suv.display_info())
+print(33 * "-")
+print(sports_car.display_info())
+print(33 * "-")
