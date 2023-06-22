30cc631f85d4ab6999e7fe48c4cca9446df2a8e5
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-22 16:51:10 +0300

    Add

--- Module №2. Python Advanced/7. Рекурсія та метакласи/Task 1.py
+++ Module №2. Python Advanced/7. Рекурсія та метакласи/Task 1.py
@@ -1,3 +1,18 @@
 
 # Завдання №1. Напишіть рекурсивну функцію, яка обчислює суму цифр заданого числа.
 
+
+def sum_digits(n):
+    d_list = [int(i) for i in str(n)]
+    return f"Сума чисел = {recursion(d_list, 0)}"
+
+
+def recursion(d_list, result):
+    if len(d_list) == 1:
+        result += d_list.pop()
+        return result
+    result += d_list.pop()
+    return recursion(d_list, result)
+
+
+print(sum_digits(12345))
