e5d2bb6e367e0c5212d6c2c3798ada81c2ead8bd
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-22 18:53:34 +0300

    Modified

--- Module №2. Python Advanced/7. Рекурсія та метакласи/Task 6.py
+++ Module №2. Python Advanced/7. Рекурсія та метакласи/Task 6.py
@@ -1,3 +1,11 @@
 
 # Завдання №6. Напишіть рекурсивну функцію, яка підраховує глибину вкладеності списків.
 
+
+def depth(lists):
+    if len(lists) == 1:
+        return 1
+    return len(lists) - 1 + depth(lists.pop())
+
+
+print(depth([1, [2, [3, [4, [5]]]]]))
