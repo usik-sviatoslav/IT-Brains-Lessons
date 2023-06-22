61f3ee1e339d9d08ec1e6005b28aef70db19214e
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-22 18:06:48 +0300

    Modified

--- Module №2. Python Advanced/7. Рекурсія та метакласи/Task 3.py
+++ Module №2. Python Advanced/7. Рекурсія та метакласи/Task 3.py
@@ -1,3 +1,11 @@
 
 # Завдання №3. Напишіть рекурсивну функцію, яка перевертає вхідний рядок.
 
+
+def reverse_str(string):
+    if len(string) == 0:
+        return string
+    return string[len(string) - 1:] + reverse_str(string[:-1])
+
+
+print(reverse_str("Hello"))
