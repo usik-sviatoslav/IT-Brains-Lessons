e6efb92be5a7c9aac91cf0151112c05a0da981d9
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-22 20:34:14 +0300

    Modified

--- Module №2. Python Advanced/7. Рекурсія та метакласи/Task 2.py
+++ Module №2. Python Advanced/7. Рекурсія та метакласи/Task 2.py
@@ -1,3 +1,15 @@
 
 # Завдання №2. Створіть метаклас, який записує в консоль повідомлення, коли створюється новий клас.
 
+
+class LogginMeta(type):
+    def __new__(cls, name, bases, attrs):
+        print(f'Class "{name}" has been created')
+        return super().__new__(cls, name, bases, attrs)
+
+
+class MyClass(metaclass=LogginMeta):
+    pass
+
+
+a = MyClass()
