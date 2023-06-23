3a1fda582bd03b7782b702d03da03750afee4a7a
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-06-23 05:53:08 +0300

    Modified

--- Module №2. Python Advanced/7. Рекурсія та метакласи/Task 4.py
+++ Module №2. Python Advanced/7. Рекурсія та метакласи/Task 4.py
@@ -2,3 +2,17 @@
 # Завдання №4. Створіть метаклас, який викидає помилку при спробі створити клас з атрибутами, що
 # починаються з '__' (два нижніх підкреслення)
 
+
+class NoDunderAttributes(type):
+    def __init__(cls, name, bases, attrs):
+        import re
+        for key in attrs:
+            if re.compile(r"^_\w+__\w+$").search(key):
+                raise TypeError('It is impossible to have attribute names beginning with "__"')
+
+        attrs["secret_attribute"] = "secret_attribute"
+        super().__init__(name, bases, attrs)
+
+
+class MyPrivateClass(metaclass=NoDunderAttributes):
+    __secret_attribute = 15
