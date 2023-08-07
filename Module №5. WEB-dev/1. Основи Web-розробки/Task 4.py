601817905276d1ce6cbd9aa7b135bc3c4993b3e3
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-08-07 04:07:17 +0300

    Modified

--- Module №5. WEB-dev/1. Основи Web-розробки/Task 4.py
+++ Module №5. WEB-dev/1. Основи Web-розробки/Task 4.py
@@ -4,28 +4,37 @@
 # за допомогою програми перегляду зображень на вашому комп'ютері.
 
 import os
+import re
 import requests
 import api_key
 
 
 def get_image(key, img):
-    response = requests.get(f"https://api.pexels.com/v1/search?query={img}", headers={"Authorization": key.image})
+    url = f"https://www.googleapis.com/customsearch/v1?q={img}&cx={key.search_eng}&key={key.token}&searchType=image"
+    response = requests.get(url)
     api_response = response.json()
 
-    if len(api_response["photos"]) != 0:
-        image_url = api_response["photos"][0]["src"]["large2x"]
-        image_name = api_response["photos"][0]["alt"]
-        image = requests.get(image_url)
+    try:
+        image_url = api_response["items"][0]["link"]
+    except KeyError:
+        return f'Не зміг знайти картинки по запиту "{img}"'
 
-        save_path = os.path.join("C:\\Users\\Usik Sviatoslav\\Downloads\\Internet", f"{image_name}.jpg")
-        with open(save_path, 'wb') as file:
-            file.write(image.content)
-        print(f'Зображення збережено у {save_path}.')
-    else:
-        print("По вашому запиту нічого не знайдено")
+    try:
+        name = api_response["spelling"]["correctedQuery"]
+    except KeyError:
+        name = img
+
+    image = requests.get(image_url)
+    filename = image_url.split(".")[-1]
+    file_type = re.findall(r"^\w+", filename).pop()
+
+    save_path = os.path.join("C:\\Users\\Usik Sviatoslav\\Downloads\\Internet", f"{name}.{file_type}")
+    with open(save_path, "wb") as file:
+        file.write(image.content)
+    return f"Зображення збережено у {save_path}."
 
 
 while True:
     user_input = input("Введи яку картинку ти хочеш отримати: ")
-    get_image(api_key, user_input)
+    print(get_image(api_key, user_input))
     print()
