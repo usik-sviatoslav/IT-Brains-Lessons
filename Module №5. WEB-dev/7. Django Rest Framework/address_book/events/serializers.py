56db20ab012d895bc62395a490710c44688dbbd3
usik-sviatoslav <usik.sviatoslav.qa@gmail.com>
2023-11-02 18:56:52 +0200

    Modified

--- Module №5. WEB-dev/7. Django Rest Framework/address_book/events/serializers.py
+++ Module №5. WEB-dev/7. Django Rest Framework/address_book/events/serializers.py
@@ -6,19 +6,18 @@ from events.models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     contacts = ContactSerializer(many=True)
-    # fields = ('id', 'first_name', 'last_name'),
+
+    class Meta:
+        model = Event
+        fields = '__all__'
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if 'contacts' in data:
-            fields = ['id', 'first_name', 'last_name']
+            fields = ['first_name', 'last_name', 'phone']
 
             data['contacts'] = [
                 {key: value for key, value in contact.items() if key in fields}
                 for contact in data['contacts']
             ]
         return data
-
-    class Meta:
-        model = Event
-        fields = '__all__'
